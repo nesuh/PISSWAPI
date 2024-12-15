fa048337478fe635bba2e7b119d8b368c1272d27
nesuh <antenhesileshi@gmail.com>
2024-12-15 11:14:06 +0300

    fix(PSR) slack-report-01

--- slack-report-back/src/modules/service/slack.service.ts
+++ slack-report-back/src/modules/service/slack.service.ts
@@ -4,7 +4,8 @@ import * as schemas from 'src/db/schemas';
 import { slackReportSendMessageDto } from "../dto/slack.dto";
 import { slack_reports } from "src/db/schemas/slack_reports.schema";
 import { WebClient } from "@slack/web-api";
-
+import { ReportStatus } from "src/shared/enums";
+import { eq } from "drizzle-orm";
 @Injectable()
 export class SlackService {
   private readonly slackClient: WebClient;
@@ -14,18 +15,23 @@ export class SlackService {
     protected readonly db: NodePgDatabase<typeof schemas>,
   ) {
     const slackToken = process.env.SLACK_BOT_TOKEN;
+    if (!slackToken || typeof slackToken !== 'string') {
+      throw new Error('Slack token is invalid or missing.');
+    }
     this.slackClient = new WebClient(slackToken);
   }
 
   async sendMessage(
     message: slackReportSendMessageDto
   ): Promise<string> {
-  
+    if (!this.slackClient) {
+      throw new Error('Slack token is invalid or missing.');
+    }
     const store = await this.db.insert(slack_reports).values({
       reportName: message.reportName,
       generatedAt: new Date(), 
       reportData: message.reportData, 
-      status:'Pending',
+      status:ReportStatus.Pending,
       text:message.text || null,
       errorMessage: message.errorMessage || null, 
     })
@@ -47,11 +53,11 @@ if(!store){
       return `Message sent successfully to Slack. Message ID: ${slackResponse.ts}`;
     } catch (error) {
 
-      // await this.db.update(slack_reports).set({
-      //   status:'Failed',
-      //   errorMessage:error.message
-      // }).where({id:store.id})
-
+      await this.db.update(slack_reports).set({
+        status: ReportStatus.Failed,
+        errorMessage: error.message,
+      }).where(eq(slack_reports.id, store.id));
+      
       console.error('Error sending Slack message:', error);
       return `Failed to send message to Slack: ${error.message}`;
     }
